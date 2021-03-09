0adfdd8b8172d17d4c459541d87782ed3a74f121
Alex Habich <me@alexnader.dev>
2021-03-09 15:07:11 -0600

    Update Firebase emulator settings

--- lib/src/backend/services/firebase.dart
+++ lib/src/backend/services/firebase.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:fluent/src/backend/models/user.dart';
+import 'package:flutter/foundation.dart';
 
 Future<void> init({bool useEmulators = false}) async {
   var core = await Firebase.initializeApp();
@@ -16,7 +17,9 @@ Future<void> init({bool useEmulators = false}) async {
   if (useEmulators) {
     auth.useEmulator('http://localhost:9099');
     firestore.settings = Settings(
-      host: "http://localhost:8080",
+      host: defaultTargetPlatform == TargetPlatform.android
+          ? '10.0.2.2:8080'
+          : 'localhost:8080',
       sslEnabled: false,
     );
     functions.useFunctionsEmulator(origin: 'http://localhost:5001');
