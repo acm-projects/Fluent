3ee861024af09720e052c192fb6747f54d77825a
Laura <lmv180001@utdallas.edu>
2021-03-13 19:09:15 -0600

    put card around login info and added background image

--- lib/src/frontend/widgets/LoginPage.dart
+++ lib/src/frontend/widgets/LoginPage.dart
@@ -1,3 +1,4 @@
+import 'package:fluent/src/frontend/widgets/MyTextField.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -21,77 +22,112 @@ class _LoginPageState extends State<LoginPage> {
           }
         },
         child: Scaffold(
-          // general padding for the screen
-          body: Center(
-            child: Padding(
-            padding: EdgeInsets.fromLTRB(20.0, 150.0, 20.0, 0.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                // Fluent logo
-               Center(
-                 child: Text(
-                    'Fluent',
-                     style: TextStyle(
-                         fontFamily: 'Pacifico',
-                       fontSize: 68.0,
-                       letterSpacing: 2.0,
-                     ),
-                    ),
-               ),
-                SizedBox(height: 6.0),
-                //Email textField and heading
-                Text(
-                  'Email',
-                  style: TextStyle(
-                    fontFamily: 'Montserrat',
-                    fontSize: 16.0,
-                  ),
-                ),
-                TextField(
-                  keyboardType: TextInputType.emailAddress,
-                  style: TextStyle(
-                    fontFamily: 'Montserrat',
-                    fontSize: 14.0,
-                  ),
-                ),
-                SizedBox(height: 16.0),
-                Text(
-                  'Password',
-                  style: TextStyle(
-                    fontFamily: 'Montserrat',
-                    fontSize: 16.0,
-                  ),
-                ),
-                TextField(
-                  keyboardType: TextInputType.text,
-                  obscureText: true,
-                  style: TextStyle(
-                    fontFamily: 'Montserrat',
-                    fontSize: 14.0,
-                  ),
-                ),
-                // sign in button
-                SizedBox(height: 24.0),
-                ElevatedButton(
-                    onPressed: () {},
-                  child: Text(
-                    'Log In'
-
-                  ),
-                    style: ElevatedButton.styleFrom(
-                    primary: Colors.lightBlue[400],
-                      minimumSize: Size(330,40),
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                ),
-                ),
-              ],
-            ),
-            ),
-           ),
+          body: SingleChildScrollView(
+            child: Container(
+              decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: NetworkImage("https://images.unsplash.com/photo-1531336116302-40e29aad0016?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80"),
+                  fit: BoxFit.cover,),
         ),
+
+            // singleChildScrollView allows for you to scroll when the keyboard is visible
+              child: Center(
+                // general padding for the screen
+                child: Padding(
+                padding: EdgeInsets.fromLTRB(20.0, 130.0, 20.0, 102.0),
+                // note: stuff from Card to the end of padding is new stuff to add the card in
+                child: Card(
+                  elevation: 2,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        // Fluent logo
+                       Center(
+                         child: Text(
+                            'Fluent',
+                             style: TextStyle(
+                                 fontFamily: 'Pacifico',
+                               fontSize: 68.0,
+                               letterSpacing: 2.0,
+                             ),
+                            ),
+                       ),
+                        SizedBox(height: 6.0),
+                        //Email textField and heading
+                        //MyTextField(headerText: 'Email', keyboardType: TextInputType.emailAddress, isObscured: false),
+                        Text(
+                          'Email',
+                          style: TextStyle(
+                            fontSize: 16.0,
+                          ),
+                        ),
+                        TextField(
+                          keyboardType: TextInputType.emailAddress,
+                          style: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontSize: 14.0,
+                          ),
+                        ),
+                        SizedBox(height: 16.0),
+                        Text(
+                          'Password',
+                          style: TextStyle(
+                            fontSize: 16.0,
+                          ),
+                        ),
+                        TextField(
+                          keyboardType: TextInputType.text,
+                          obscureText: true,
+                          style: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontSize: 14.0,
+                          ),
+                        ),
+                        // sign in button
+                        SizedBox(height: 24.0),
+                        ElevatedButton(
+                            onPressed: () {},
+                          child: Text('Log in',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                            style: ElevatedButton.styleFrom(
+                            primary: Colors.lightBlue[400],
+                              minimumSize: Size(330,40),
+                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                        ),
+                        ),
+                        SizedBox(height: 10.0),
+                        Row(
+                          children: [
+                            Text('New user?'),
+                            TextButton(
+                              onPressed: () {},
+                              child: Text('Sign up now!'),
+                              style: TextButton.styleFrom(
+                                primary: Colors.lightBlue[400],
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                ),
+               ),
+              ),
+            ),
+         ),
       ),
     );
 
   }
-}
+
+  }
+
