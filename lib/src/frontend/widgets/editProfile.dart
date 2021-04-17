f9d3ffa7ca8ec6652662c6a55b794a5b2cdf7948
Gary Wu <gyw200000@utdallas.edu>
2021-04-16 23:07:40 -0500

    updated

--- lib/src/frontend/widgets/editProfile.dart
+++ lib/src/frontend/widgets/editProfile.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 import 'package:fluent/src/backend/models/fluency.dart';
+import 'package:fluent/src/backend/models/match.dart';
 import 'package:fluent/src/backend/services/base/services.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fluent/src/frontend/pages.dart';
+import 'package:fluent/src/frontend/widgets/LoggedInUserNavigation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
@@ -135,7 +137,18 @@ class _EditProfilePageState extends State<EditProfilePage> {
             Icons.arrow_back,
             color: Colors.lightBlueAccent,
           ),
-          onPressed: () {Navigator.pop(context);},
+          onPressed: () async {
+            final matching = ServicesProvider.of(context).services.matching;
+            MatchProfile user = await matching.getUserData(FirebaseAuth.instance.currentUser.uid);
+
+              //Navigator.pushNamed(context,"/navigation");
+              Navigator.push(context,
+                  MaterialPageRoute(
+                      builder: (context) => BottomNavBar(
+                          pfp: user.pfp
+                      )
+                  ));
+          },
         ),
       ),
       body: Container(
